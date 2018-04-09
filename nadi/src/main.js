67fe7909843128b26a054d74489014596f90a3f1
specupJeongmin <jeongmin.lee@specupad.com>
2018-04-09 17:07:39 +0900

    다음 지도 api, lodash, at-ui 추가

--- nadi/src/main.js
+++ nadi/src/main.js
@@ -3,7 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import AtComponents from 'at-ui'
+import 'at-ui-style'    // Import CSS
 
+// import 'at-ui-style/src/index.scss'      // Or import the unbuilt version of SCSS
+
+Vue.use(AtComponents)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
