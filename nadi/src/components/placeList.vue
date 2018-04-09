1b19bded6376df2041e8b04d123ae77ab29f8302
이정민 <>
2018-04-10 00:50:23 +0900

    키워드 목록 스타일 수정

--- nadi/src/components/placeList.vue
+++ nadi/src/components/placeList.vue
@@ -3,9 +3,15 @@
     <keyword @search="search"></keyword>
     <div class="list" v-infinite-scroll="loadMore">
       <at-card v-for="p in placeList" :key="p.id">
-        <h4 slot="title">{{ p.place_name }}</h4>
+        <h4 slot="title">{{ p.place_name }}
+          <a :href="p.place_url" target="_blank">
+            <i class="icon icon-external-link"></i>
+          </a>
+        </h4>
         <div>
-          Card Content
+          <p class="category">{{ p.category_name }}</p>
+          <p class="addr">{{ p.address_name }}</p>
+          <p><a :href="`tel:${p.phone}`">{{ p.phone }}</a></p>
         </div>
       </at-card>
     </div>
@@ -28,6 +34,7 @@ export default {
   directives: { infiniteScroll },
   methods: {
     search ({ data, pagination }) {
+      console.log(data[0])
       this.pagination = pagination
       if (pagination.current === 1) {
         this.placeList = data
@@ -46,12 +53,26 @@ export default {
 
 <style>
 #menu_wrap {position:absolute;top:0;left:0;bottom:0;width:250px;margin:10px 0 30px 10px;padding:5px;overflow-y:auto;background:rgba(255, 255, 255, 0.7);z-index: 1;font-size:12px;border-radius: 10px;}
-.list .at-card {
+#menu_wrap .list .at-card {
   width: 100%;
-  margin-top: 10px;
+  margin-top: 5px;
+  background:rgba(255, 255, 255, 0.3);
+  border: 1px solid rgba(136, 129, 129, 0.3);
 }
-.list .at-card .at-card__head {
+#menu_wrap .list .at-card .at-card__body {
+  padding: 12px 24px;
+}
+#menu_wrap .list .at-card .at-card__body p {
   text-align: left;
+  font-size: 12px;
+  margin-bottom: 5px;
+}
+
+#menu_wrap .list .at-card .at-card__body a {
+  color: #13ce66;
+}
+#menu_wrap .list .at-card .at-card__body .category {
+  font-size: 11px;
 }
 
 
